002dda30efb420b3f32e8e7a60a1af6003bcaef4
alisharayna <alisharaynaazzahra138@gmail.com>
2025-06-02 11:49:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.title("dey ha'ah lah")
-st.write(
-eng ing eng [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-
-st.image (8cb2bf0306bf1650ddf37ebe4d993aaa (1).jpg, width 200)
+st.write("eng ing eng")
+st.image ("./8cb2bf0306bf1650ddf37ebe4d993aaa (1).jpg", width=200)
+st.write("\n")
+st.subheader("eak")
+st.write("abubu lucu")
